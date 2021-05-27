580ff86e7d155a2549698ce5026db70d3257f462
Damjan Skulic <52981481+SkulicDamian@users.noreply.github.com>
2021-05-27 17:54:19 -0400

    Adding work done
    Online Class I took to get started using OpenCv

---
+++ warp_perspective.py
@@ -0,0 +1,16 @@
+j
+
+
+width,height = 250,350
+pts1 = np.float32([[45,37],[127,29],[73,145],[118,153]])   ##got the coordinate from opening paint and getting the number from there
+pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+matrix = cv2.getPerspectiveTransform(pts1,pts2)
+imgOutput = cv2.warpPerspective(img,matrix,(width,height))
+
+
+
+
+cv2.imshow('Image', img)
+cv2.imshow("Output",imgOutput)
+
+cv2.waitKey(0)
